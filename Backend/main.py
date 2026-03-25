4aa84838038fa1005aabe758dee73c8ef94089b0
Daniel160490 <danigd71@gmail.com>
2026-03-25 12:42:27 +0100

    Corrección de CORS

--- Backend/main.py
+++ Backend/main.py
@@ -13,13 +13,13 @@ app = FastAPI()
 origins = [
     "http://localhost:5173", 
     "http://127.0.0.1:5173",
-    "https://grumpis-game.vercel.app/"
+    "https://grumpis-game.vercel.app"
 ]
 
 # CONFIGURACIÓN DE CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
